db264fe22568464efe973100d48f598291f94d22
Riya kaushik <riyakaushik6410@gmail.com>
2025-11-16 21:16:52 +0530

    login successfully built

---
+++ frontend/src/components/AuthLayout.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const AuthLayout = ({children}) => {
+  return (
+    <div className='flex h-screen overflow-hidden'>
+      <div className='w-full md:w-1/2 overflow-y-auto'>
+        <div className='min-h-screen flex px-12 pt-8 pb-12 flex-col'>
+            <div className='flex flex-grow justify-center items-center'>
+                {children}
+            </div>
+        </div>
+      </div>
+
+      <div className='hidden md:block w-1/2'>
+        <img 
+          src="https://images.pexels.com/photos/3183153/pexels-photo-3183153.jpeg"
+          alt="login background" 
+          className='h-full w-full object-cover'/>
+      </div>
+    </div>
+  )
+}
+
+export default AuthLayout
